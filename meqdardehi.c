25ed528adac22eba0251973f25fa2786d943fdae
ASUS <pry.tvl.zed@gmail.com>
2024-12-04 17:34:21 +0330

    update

--- meqdardehi.c
+++ meqdardehi.c
@@ -4,18 +4,19 @@
 #include "stdio.h"
 #include "meqdardehi.h"
 extern int map[17][17];
-void maqar(){
+void maqar(int k,int j){
     int maqarnum;
+    int x,y;
     printf("inter maqar's number please:");
     scanf("%d",&maqarnum);
-    int x,y;
     for(int i = 1;i <= maqarnum;i++){
         printf("x,y %d ra vard konid",i);
         scanf("%d %d",&x,&y);
-       map[x][y]='c';
+        if(k<x||j<y) i--;
+       else map[x][y]='c';
     }
 }
-void rosta(){
+void rosta(int k,int j){
     int RoostaNum;
     printf("inter Roosta's number please:");
     scanf("%d",&RoostaNum);
@@ -23,11 +24,11 @@ void rosta(){
     for (int i = 0; i < RoostaNum ; ++i) {
         printf("x,y%d ra vard konid",i+1);
         scanf("%d %d",&x,&y);
-        if(map[x][y]!='c')
+        if(map[x][y]!='c'&& k>x && j>y)
         map[x][y]='v';
         else i--;
     }}
-void masdod(){
+void masdod(int k,int j){
     int x,y;
     int MasdodNum;
     printf("tedadmasdod ra vard konid");
@@ -35,11 +36,11 @@ void masdod(){
     for (int i = 0; i < MasdodNum ; ++i) {
         printf("x,y%d ra vard konid",i+1);
         scanf("%d %d",&x,&y);
-        if(map[x][y]!='c'&&map[x][y]!='v')
+        if(map[x][y]!='c'&&map[x][y]!='v'&&k>x&&j>y)
         map[x][y]='x';
     else i--;}}
 
-    void khali(){
+    void khali(int k , int i){
         for (int r = 0; r < 17; ++r) {
             for (int j = 0; j <17 ; ++j) {
                 if(map[r][j]!='c'&&map[r][j]!='v'&&map[r][j]!='x'){
--- meqdardehi.h
+++ meqdardehi.h
@@ -5,8 +5,8 @@
 #ifndef UNTITLED4_MEQDARDEHI_H
 #define UNTITLED4_MEQDARDEHI_H
 extern int map[17][17];
-void maqar();
-void rosta();
-void masdod();
-void khali();
+void maqar(int x,int y);
+void rosta(int x, int y);
+void masdod(int x, int y);
+void khali(int x, int y);
 #endif
