a2a4471e1be194cc6b8b9f17e920b019fd3f35e1
Asus <bagherianfatemeh84@gmail.com>
2025-01-05 14:22:08 +0330

    gameUpdate.h was added

--- grid.c
+++ grid.c
@@ -10,21 +10,21 @@ int grid(int i, int j) {
     float horizontal_spacing = 68;
 
     // Calculate the actual grid width and height
-    float grid_width = (i) * vertical_spacing;
-    float grid_height = (j) * horizontal_spacing;
+    float grid_width = (j) * vertical_spacing;
+    float grid_height = (i) * horizontal_spacing;
 
     // Calculate the offset for centering the grid
     float offset_x = (WINDOW_WIDTH - grid_width) / 2.0;
     float offset_y = (WINDOW_HEIGHT - grid_height) / 2.0;
 
     // Draw vertical lines
-    for (int x = 0; x <= i; x++) {
+    for (int x = 0; x <= j; x++) {
         float posX = offset_x + x * vertical_spacing;
         DrawLine(posX, offset_y, posX, offset_y + grid_height, BLACK);
     }
 
     // Draw horizontal lines
-    for (int y = 0; y <= j; y++) {
+    for (int y = 0; y <= i; y++) {
         float posY = offset_y + y * horizontal_spacing;
         DrawLine(offset_x, posY, offset_x + grid_width, posY, BLACK);
     }
--- main.c
+++ main.c
@@ -16,7 +16,7 @@ Village villages[20];
 int main() {
 
     //receiving map Height and Width
-    for (int i = 0; i < 17 ; i++) {
+    for (int i = 0; i < 17 ; ++i) {
      for(int j=0;j < 17; j++) {
          map[i][j].type=10;
          map[i][j].road=-10;
@@ -74,78 +74,78 @@ int main() {
 
         DrawTexture(Background, 0, 0, WHITE);
         grid(x, y);
-        float offsetX = (WINDOW_WIDTH - x * 68) / 2.0;
-        float offsetY = (WINDOW_HEIGHT - y * 68) / 2.0;
+        float offsetX = (WINDOW_WIDTH - y * 68) / 2.0;
+        float offsetY = (WINDOW_HEIGHT - x * 68) / 2.0;
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; ++j) {
-                Rectangle cellrect = {offsetX + i * 68, offsetY + j * 68, 68, 68
+                Rectangle cellrect = {offsetX + j * 68, offsetY + i * 68, 68, 68
                 };
                 if (map[i][j].type == 'c') {
 
-                    DrawTexture(Kingdom, offsetX + i * 68, offsetY + j * 68, WHITE);
+                    DrawTexture(Kingdom, offsetX + j * 68, offsetY + i * 68, WHITE);
 
                 } else if (map[i][j].type == 'v') {
-                    DrawTexture(Village, offsetX + i * 68, offsetY + j * 68, WHITE);
+                    DrawTexture(Village, offsetX + j * 68, offsetY + i * 68, WHITE);
 
                 } else if (map[i][j].type == 'x') {
-                    DrawTexture(ForceClosed, offsetX + i * 68, offsetY + j * 68, WHITE);
+                    DrawTexture(ForceClosed, offsetX + j * 68, offsetY + i * 68, WHITE);
                 } else if(map[i][j].type==0){
-                    DrawRectangle( offsetX + i * 68, offsetY + j * 68,68,68,RED);
+                    DrawRectangle( offsetX + j * 68, offsetY + i * 68,68,68,RED);
                 }else if(map[i][j].type==-1){
-                    DrawRectangle(offsetX + i * 68, offsetY + j * 68,68,68,WHITE);
+                    DrawRectangle(offsetX + j * 68, offsetY + i * 68,68,68,WHITE);
                 }else if(map[i][j].type==-2){
-                    DrawRectangle(offsetX + i * 68, offsetY + j * 68,68,68,GREEN);
+                    DrawRectangle(offsetX + j * 68, offsetY + i * 68,68,68,GREEN);
                 }else if(map[i][j].type==-3) {
-                    DrawRectangle(offsetX + i * 68, offsetY + j * 68, 68, 68, BLUE);
+                    DrawRectangle(offsetX + j * 68, offsetY + i * 68, 68, 68, BLUE);
                 }
                 else{
                     char text = map[i][j].type;
-                    DrawText(TextFormat("%d", text), offsetX + i * 68, offsetY + j * 68, 24, RED);
+                    DrawText(TextFormat("%d", text), offsetX + j * 68, offsetY + i * 68, 24, RED);
                 }
                 if (CheckCollisionPointRec(mouseposition, cellrect)) {
                     char infotext[50];
                     DrawRectangle(cellrect.x, cellrect.y, 68, 68, GRAY);
-                if (map[i][j].type == 'v') {
-                    for (int v = 0; v < villageCount; v++) {
-                        if (villages[v].x == i && villages[v].y == j) {
-                            sprintf(infotext, "gold: %d\nfood: %d\nowner: %d",
-                                    villages[v].GoldProduction,
-                                    villages[v].FoodProduction,
-                                    villages[v].ownerId);
-                            DrawText(infotext, cellrect.x, cellrect.y, 10, BLACK);
-                            break;
+                    if (map[i][j].type == 'v') {
+                        for (int v = 0; v < villageCount; v++) {
+                            if (villages[v].x == i && villages[v].y == j) {
+                                sprintf(infotext, "gold: %d\nfood: %d\nowner: %d",
+                                        villages[v].GoldProduction,
+                                        villages[v].FoodProduction,
+                                        villages[v].ownerId);
+                                DrawText(infotext, cellrect.x, cellrect.y, 10, BLACK);
+                                break;
+                            }
                         }
-                    }
-                } else if (map[i][j].type == 'c') {
-                    for (int c = 0; c < kingdomCount; c++) {
-                        if (kingdoms[c].x == i && kingdoms[c].y == j) {
-                            sprintf(infotext, "Coins: %d\nServes:%d\nGold:%d\nFood:%d\nWorkers:%d\nSoldier:%d",
-                                    kingdoms[c].Gold,
-                                    kingdoms[c].Serve,
-                                    kingdoms[c].GoldProduction,
-                                    kingdoms[c].FoodProduction,
-                                    kingdoms[c].WorkersCount,
-                                    kingdoms[c].soldierCount);
-                            DrawText(infotext, cellrect.x, cellrect.y, 10, BLACK);
-                            break;
+                    } else if (map[i][j].type == 'c') {
+                        for (int c = 0; c < kingdomCount; c++) {
+                            if (kingdoms[c].x == i && kingdoms[c].y == j) {
+                                sprintf(infotext, "Coins: %d\nServes:%d\nGold:%d\nFood:%d\nWorkers:%d\nSoldier:%d",
+                                        kingdoms[c].Gold,
+                                        kingdoms[c].Serve,
+                                        kingdoms[c].GoldProduction,
+                                        kingdoms[c].FoodProduction,
+                                        kingdoms[c].WorkersCount,
+                                        kingdoms[c].soldierCount);
+                                DrawText(infotext, cellrect.x, cellrect.y, 10, BLACK);
+                                break;
+                            }
                         }
                     }
                 }
-            }
 
+            }
         }
-    }
         if(IsKeyDown(KEY_R)){
             int xv=((mouseposition.y-offsetY))/68;
             int yv=((mouseposition.x-offsetX)/68);
             for (int k = 0; k < villageCount ; ++k) {
-            if(villages[k].x==xv&&villages[k].y==yv){
-            SuggestedRoad(kingdoms[currentkingdom].x,kingdoms[currentkingdom].y,k);
-            for (int i = 0; i < x ; ++i) {
-                for (int j = 0; j <y ; ++j) {
-                    if(map[i][j].road==k)DrawRectangle(offsetX + j * 68, offsetY + i * 68, 68, 68, GRAY);
-                }
-            }}}
+                if(villages[k].x==xv&&villages[k].y==yv){
+                    SuggestedRoad(kingdoms[currentkingdom].x,kingdoms[currentkingdom].y,k);
+                    for (int i = 0; i < x ; ++i) {
+                        for (int j = 0; j <y ; ++j) {
+                            if(map[i][j].road==k)DrawRectangle(offsetX + j * 68, offsetY + i * 68, 68, 68, GRAY);
+                        }
+                    }}}
 
 
         }
@@ -154,7 +154,7 @@ int main() {
         if(IsKeyPressed(KEY_ENTER)){
             showguide=!showguide;
         }
-        if(showguide) DrawTexture(guide,0,500,WHITE);
+        if(showguide) DrawTexture(guide,500,0,WHITE);
 
         if (IsKeyPressed(KEY_ONE)) {
 
@@ -188,8 +188,8 @@ int main() {
         }
 
 
-    EndDrawing();
-}
+        EndDrawing();
+    }
     CloseWindow();
-return 0;
+    return 0;
 }
